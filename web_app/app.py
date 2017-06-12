016ad38c25a08d274e05b6fb09075f9d184f14a2
Bob Corboy <r.corboy23@gmail.com>
2017-06-12 14:33:39 -0600

    start web app, start customizing for project

---
+++ web_app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import sys
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/positions')
+def positions():
+    return render_template('positions.html')
+
+@app.route('/motivation')
+def hello():
+    return render_template('motivation.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/model')
+def problem():
+    return render_template('model.html')
+
+@app.route('/teamneeds', methods=['GET','POST'])
+def score():
+    return render_template('score_prompt.html', predicted=y)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
